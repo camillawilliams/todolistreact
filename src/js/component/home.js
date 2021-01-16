5b0262e89b0f42d592d521237194b9be38c30f88
camillawilliams <camillawilliamsdu@gmail.com>
2021-01-16 00:17:37 +0000

    copied todo list for fetch add

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+//todo list fetch add
 //create your first component
-export function Home() {
+export const Home = () => {
+	const [theList, setList] = useState([
+		"do homework",
+		"take out garbage",
+		"go to the mall"
+	]);
+	const [userInput, setUserInput] = useState("");
+
+	const handleKeyUp = event => {
+		if (event.keyCode == 13 && userInput != "") {
+			setList(theList.concat(userInput));
+			setUserInput("");
+		}
+	};
+	const itemDelete = index => {
+		var updatedList = theList.filter(
+			(task, taskIndex) => index != taskIndex
+		);
+		setList(updatedList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="text-center">
+				<h1>To Do List</h1>
+				<div className="todoList">
+					<input
+						className="todoInput"
+						onChange={event => setUserInput(event.target.value)}
+						value={userInput}
+						onKeyUp={handleKeyUp}
+					/>
+					<ul className="list-group list-group-flush">
+						{theList.map((value, index) => {
+							return (
+								<li className="list-group-item" key={index}>
+									{value}
+									<button
+										type="button"
+										onClick={() => itemDelete(index)}
+										className="btn btn-primary">
+										X
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
-}
+};
